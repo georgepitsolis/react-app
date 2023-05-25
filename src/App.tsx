69bceb08ba777f49683bc2eb84003d5ecd91aaa6
georgepitolis <el16441@central.ntua.gr>
2023-05-25 22:17:02 +0300

    final commit from simple tutorial on react

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+
+// import ListGroup from "./components/ListGroup";
 let items = ["New York", "Athens", "Tokio", "London", "Paris"];
 
 function App() {
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      {/* <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      /> */}
+      {/* <Alert>
+        Hello <span></span> World
+      </Alert> */}
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>Primary Button</Button>
     </div>
   );
 }
